ce5c789f76c395f36211daaa3b5ab297112cfe7d
Jaichandu007 <jaichandujc007@gmail.com>
2022-06-24 16:52:21 +0530

    j

---
+++ Training_Assignments/OOPs /Points.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Points {
+    int X;
+    int Y;
+    Scanner input = new Scanner(System.in);
+        public void vertices(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    
+        // public vertices(int X)
+        // {
+        //     X = input.nextInt(); 
+        //     return X;
+        // }
+        // public Points(int Y)
+        // {
+        //     Y = input.nextInt();   
+        //     return Y ;
+        // }
+
+
+        // {
+        //     this.X = X;
+        //     this.Y = Y;
+        // }
+         public void setX(int X)
+         {
+            this.X = X;
+         }
+
+        int getX()
+        {
+            return X;
+        }
+
+        public void setY(int Y)
+        {
+            this.Y = Y;
+        }
+
+        public int getY()
+        {
+            return Y;
+        }
+    }
+
