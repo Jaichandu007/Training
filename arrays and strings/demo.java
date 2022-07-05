ba5686b3d8df2ee0f4a6589b0f318dca33e13721
Jaichandu007 <jaichandujc007@gmail.com>
2022-07-05 14:58:25 +0530

    hi

---
+++ arrays and strings/demo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.String
+class ex{
+    public static void main(String[] args){
+        String str = null;
+        String str1="abc";
+        System.out.println(str1.equals("abc") | str.equals(null));
+
+    }
+}
